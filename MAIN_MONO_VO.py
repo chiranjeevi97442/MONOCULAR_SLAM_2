a68372c8b839f98aa886e3fdc9c4c4fb34b11638
chiranjeevi97442 <achiranjeevi37@gmail.com>
2024-10-29 21:45:49 +0530

    Update MAIN_MONO_VO.py
    Adding some functions for the frame ORD detection and all

--- MAIN_MONO_VO.py
+++ MAIN_MONO_VO.py
@@ -3,9 +3,6 @@
 Created on Tue Oct 29 07:51:35 2024
 
 @author: chiranjeevi
-Cuddammm
-Cuddammm2222
-CUUUUUUUUUU
 
 """
 
@@ -13,14 +10,34 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
+
+
+
+def process_frames(img_list):
+    
+    I1 = cv2.cvtColor(img_list[0], cv2.COLOR_BGR2GRAY)
+    I2 = cv2.cvtColor(img_list[1], cv2.COLOR_BGR2GRAY)
+
+
+
+
+
+
 def main():
     IMG_LIST=[]
     CAP=cv2.VideoCapture("SPECIFY_THE_PATH")
+    
     while True:
         ret,frame=CAP.read()
         if ret:
             IMG_LIST.append(frame)
+            if len(IMG_LIST)==2:
+                process_frames(IMG_LIST)
+            else:
+                continue
+            IMG_LIST.pop(0)
             cv2.imshow("DISPLAYING_IMAGE",frame)
+            
             if cv2.waitKey(1)==ord('q'):
                 break
     CAP.release()
