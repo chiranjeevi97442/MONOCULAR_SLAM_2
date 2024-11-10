dcfd65b1b81b91ec84bb3dfab9f36791a1abe356
chiranjeevi97442 <achiranjeevi37@gmail.com>
2024-11-10 11:45:29 +0530

    Update MAIN_MONO_VO.py
    another feature_matcher GOOD_FEATURE_MATCHES AND ALL

--- MAIN_MONO_VO.py
+++ MAIN_MONO_VO.py
@@ -14,12 +14,25 @@ import os
 
 def BF_FeatureMatcher(des1,des2):
     brute_force = cv2.BFMatcher(cv2.NORM_HAMMING,crossCheck=True)
-    #no_of_matches = brute_force.match(des1,des2)
-    no_of_matches = brute_force.knnMatch(des1,des2,k=2)
+    no_of_matches = brute_force.match(des1,des2)
+    #no_of_matches = brute_force.knnMatch(des1,des2,k=2)
  
     # finding the humming distance of the matches and sorting them
     no_of_matches = sorted(no_of_matches,key=lambda x:x.distance)
     return no_of_matches
+
+
+    
+def feature_mapping(img):
+    orb=cv2.ORB_create()
+    
+    pts=cv2.goodFeaturesToTrack(img,1000, qualityLevel=0.01, minDistance=7)
+    key_pts=[cv2.KeyPoint(x=f[0][0], y=f[0][1], size=20) for f in pts]
+    key_pts,descriptors=orb.compute(img,key_pts)
+    
+    return np.array([(kp.pt[0],kp.pt[1]) for kp in key_pts]),descriptors,orb
+            
+     
 
 def process_frames(img_list):
     
@@ -37,16 +50,52 @@ def process_frames(img_list):
     
     
     good_matches=BF_FeatureMatcher(desc_1, desc_2)
+    #print("good_matches",good_matches[:10])
     tot_good_matches=len(good_matches)
+    print("key_pt_1",key_pt_1)
     out_put_image=cv2.drawMatches(I1, key_pt_1, I2, 
             key_pt_2, good_matches[:10], None, flags=2)
-    print("length of key_pt_1 and key_pt_2",len(key_pt_1),len(key_pt_2))     
+    #print("length of key_pt_1 and key_pt_2",len(key_pt_1),len(key_pt_2))     
     I1=cv2.drawKeypoints(I1,key_pt_1, I1)
     I2=cv2.drawKeypoints(I2,key_pt_2, I2)
-    print("total number of matches",tot_good_matches)
+    #print("total number of matches",tot_good_matches)
     
     return I1,I2,out_put_image
 
+def process_frames_1(img_list):
+   
+    I1 = cv2.cvtColor(img_list[0], cv2.COLOR_BGR2GRAY)
+    I2 = cv2.cvtColor(img_list[1], cv2.COLOR_BGR2GRAY)
+    key_pts_1,des_1,orb=feature_mapping(I1)
+    
+    key_pts_2,des_2,orb=feature_mapping(I2)
+    #print(len(key_pts_1),len(key_pts_2))
+    #print(len(des_1),len(des_2))
+    
+    
+    def GOOD_MATCHES(des1,des2):
+        bf=cv2.BFMatcher(cv2.NORM_HAMMING)
+        matches=bf.knnMatch(des1,des2,k=2)
+        GOOD=[]
+        for m,n in matches:
+            if m.distance < 0.75*n.distance:
+                GOOD.append([m])
+                                
+        return GOOD
+    
+    good_matches=GOOD_MATCHES(des_1, des_2)
+    #print("good_matches",len(good_matches))
+    #print("lenght of key_pts_1",len(key_pts_1))
+    #print("lenght of key_pts_2",len(key_pts_2))
+    print("key_pt_1",key_pts_1)
+    output=cv2.drawMatches(I1, key_pts_1, I2, key_pts_2, good_matches[:10], None, flags=2)
+    
+    I1=cv2.drawKeypoints(I1,key_pts_1, I1)
+    I2=cv2.drawKeypoints(I2,key_pts_2, I2)
+    
+    
+    return I1,I2,output
+    
 
 
 
@@ -60,7 +109,7 @@ def main():
         if ret:
             IMG_LIST.append(frame)
             if len(IMG_LIST)==2:
-                F1,F2,output=process_frames(IMG_LIST)
+                F1,F2,output=process_frames_1(IMG_LIST)
             else:
                 continue
             IMG_LIST.pop(0)
